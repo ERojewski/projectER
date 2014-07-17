d1d14dd9522f4d5d2cc04b9f4467296a27ada4a2
Emil <emil_ro@o2.pl>
2014-07-17 21:56:09 +0200

    Change perspective for comments

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-#encoding UTF-8
-
-class Comment < ActiveRecord::Base
-	validates :comment_content, presence: true
-	belongs_to :user
-end
--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -1,4 +1,12 @@
 require "rails_helper"
 !
-RSpec.describe PostController, type: :controller do
+RSpec.describe PostsController, type: :controller do
+	
+	describe "GET #index" do
+	end
+
+	it 'redirects after creater post' do
+		post :create, post: {content: "post"}
+		expect(response).to redirect_to(animals_path)
+	end
 end
